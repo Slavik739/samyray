c3bcea4130d6e94dfedd0e49673348c497f500d3
Slavik739 <Svstason@gmail.com>
2021-03-17 03:00:15 +0200

    schema first page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
-import Footer from "./Footer";
+
 
 const App = () => {
     return (
-        <div>
-           <Header />
-           <Technologies />
-           <Footer/>
+        <div className='app-wrapper'>
+           <header className='header'>
+               <img className='logo' src="https://www.crazyhippos.in/wp-content/uploads/2018/02/logo-23.jpg" alt="logo"/>
+           </header >
+           <nav className='nav'>
+               <ul>
+                   <li><a href="#">Profile</a></li>
+                   <li><a href="#">Messages</a></li>
+                   <li><a href="#">News</a></li>
+                   <li><a href="#">Music</a></li>
+                   <li><a href="#">Settings</a></li>
+               </ul>
+           </nav>
+            <div className='content'>
+                <div>
+                <img src="https://thumbs.dreamstime.com/b/butterfly-flower-23417093.jpg" alt=""/>
+                </div>
+                <div>
+                    ava+description</div>
+                <div>
+                    my post
+                    <div>
+                        new post
+                    </div>
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+
         </div>
     );
 };
