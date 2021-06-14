73c041834a94e12b4f41c141194e850b960f530d
Stanislav <Svstason@gmail.com>
2021-06-15 00:25:03 +0300

    lesson 16. added props for Componenta - Post.

--- src/components/Profile/MyPost/MyPost.jsx
+++ src/components/Profile/MyPost/MyPost.jsx
@@ -2,21 +2,20 @@ import React from 'react'
 import s from './MyPost.module.css'
 import Post from "./Post/Post";
 
-const MyPost = () => {
+const MyPost = (props) => {
+
     return <div className={s.myPostAll}>
 
         <div className={s.myPost}>
             <p className={s.myPost__header}>My posts</p>
-            <textarea className={s.myPost__input} name=""  cols="50" rows="10"></textarea>
+            <textarea className={s.myPost__input} name="" cols="50" rows="10"></textarea>
             <button className={s.myPost__btn}>Add post</button>
         </div>
         <div className={s.posts}>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
+            <Post massage="hi, how are you?" like="4"/>
+            <Post massage="It's my first post" like="23"/>
+            <Post massage="I'm in work now" like="14"/>
+            <Post massage="Lorem" like="0"/>
 
         </div>
     </div>
--- src/components/Profile/MyPost/Post/Post.jsx
+++ src/components/Profile/MyPost/Post/Post.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import s from './Post.module.css'
 
-const Post = () => {
+const Post = (props) => {
     return <div>
         <div className={s.item}>
             <div>
                 <img className={s.logoImage} src="https://www.crazyhippos.in/wp-content/uploads/2018/02/logo-23.jpg"
                      alt=""/>
                 <div>
-                    <span>like</span>
+                    <span>like - {props.like}</span>
                 </div>
             </div>
-            <div className={s.text}>post 1</div>
+            <div className={s.text}>{props.massage}</div>
         </div>
 
     </div>
