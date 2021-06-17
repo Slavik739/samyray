b4837c15e1fc58e0d1ef1e76626928a726fc97de
Stanislav <Svstason@gmail.com>
2021-06-17 11:22:14 +0300

    lesson 21. Added page Dialog

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,20 @@ import s from './Dialogs.module.css'
 const Dialogs = () => {
   return (
       <div className={s.wrapper}>
-        Dialogs
+        <div className={s.dialogs}>
+          <div className={s.dialogsItems}>
+            <div className={s.dialog+' '+s.active}>Dmitriy</div>
+            <div className={s.dialog}>Andrey</div>
+            <div className={s.dialog}>Svetlana</div>
+            <div className={s.dialog}>Alisa</div>
+            <div className={s.dialog}>Petty</div>
+          </div>
+        </div>
+        <div className={s.massages}>
+          <div className={s.massage}>Hello</div>
+          <div className={s.massage}>How are you?</div>
+          <div className={s.massage}>I'm see sea</div>
+        </div>
       </div>
   )
 }
