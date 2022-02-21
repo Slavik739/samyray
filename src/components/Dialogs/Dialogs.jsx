da6881353548a431e079f8073beebe79de2f3bb4
Stanislav <Svstason@gmail.com>
2022-02-21 20:35:58 +0200

    Lesson 24. Data in object

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,24 +2,38 @@ import React from 'react'
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+const DialogItem = (props) => {
+	return <div className={s.dialog}>
+		<NavLink to={"/dialogs/" + props.id}
+						 className={props.active === 'active' ? s.active : ""}>
+			{props.name}
+		</NavLink>
+	</div>
+}
+const Message = (props) => {
+	return <div className={s.massage}>{props.message}</div>
+}
 
 const Dialogs = () => {
-	const DialogItem = (props) => {
-		return <div className={s.dialog}>
-			<NavLink to={"/dialogs/" + props.id}
-							 className={props.active === 'active' ? s.active : ""}>
-				{props.name}
-			</NavLink>
-		</div>
-	}
-	const Message = (props) => {
-		return <div className={s.massage}>{props.message}</div>
-	}
+	let dialogsData = [
+		{id: 1, name: "Dimuch"},
+		{id: 2, name: "Andrey"},
+		{id: 3, name: "Svetlana"},
+		{id: 4, name: "Alisa"},
+		{id: 5, name: "Petty~"}
+	]
+	let messagesData = [
+		{id: 1, message: "Hello"},
+		{id: 2, message: "How are you?"},
+		{id: 3, message: "I'm see sea"},
+		{id: 4, message: "Yo"},
+		{id: 5, message: "Yo~"}
+	]
 	return (
 		<div className={s.wrapper}>
 			<div className={s.dialogs}>
 				<div className={s.dialogsItems}>
-					<DialogItem name="Dimuch" id="1" active="active"/>
+					<DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
 					<DialogItem name="Andrey" id="2"/>
 					<DialogItem name="Svetlana" id="3"/>
 					<DialogItem name="Alisa" id="4"/>
--- src/components/Profile/MyPost/MyPost.jsx
+++ src/components/Profile/MyPost/MyPost.jsx
@@ -3,7 +3,12 @@ import s from './MyPost.module.css'
 import Post from "./Post/Post";
 
 const MyPost = () => {
-
+	let postData = [
+		{id: 1, message: "hi, how are you?", likesCount: 4},
+		{id: 2, message: "It's my first post", likesCount: 45},
+		{id: 3, message: "I'm in work now", likesCount: 12},
+		{id: 4, message: "Lorem", likesCount: 0},
+	]
 	return <div className={s.myPostAll}>
 		<div className={s.myPost}>
 			<h2 className={s.myPost__header}>My posts</h2>
@@ -11,7 +16,8 @@ const MyPost = () => {
 			<button className={s.myPost__btn}>Add post</button>
 		</div>
 		<div className={s.posts}>
-			<Post massage="hi, how are you?" likesCount="4"/>
+			<Post massage={postData[0].message} likesCount={postData[0].likesCount}/>
+			<Post massage={postData[1].message} likesCount={postData[1].likesCount}/>
 			<Post massage="It's my first post" likesCount="23"/>
 			<Post massage="I'm in work now" likesCount="14"/>
 			<Post massage="Lorem" likesCount="0"/>
